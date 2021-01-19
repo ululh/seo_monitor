d635ab9e985107063bba2d9c0d47c5d2489a7d8b
Ulul <ulul@ulul.org>
2021-01-19 21:24:44 +0100

    restart always graf mysql

--- grafana_mysql/mysql_scripts/initial_load.sh
+++ grafana_mysql/mysql_scripts/initial_load.sh
@@ -6,6 +6,6 @@ mysql -uroot -p$PASS < /mysql_scripts/create_table_mdr.sql
 for file in $(ls /var/lib/mysql/CSVs)
 do
 	sed -e "s/TOTO/$file/" /mysql_scripts/load_csv.sql.template > /mysql_scripts/load_csv.sql
-	#cat /mysql_scripts/load_csv.sql
+	cat /mysql_scripts/load_csv.sql
 	mysql -uroot -p$PASS < /mysql_scripts/load_csv.sql
 done
